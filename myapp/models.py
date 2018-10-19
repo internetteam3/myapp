ad7a719f6f2397f03b50cc3e0833b2b0d8ab50b7
Neel Shah <neel.4747@gmail.com>
2018-10-19 18:22:02 -0400

    Changing model attributes

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
 
+
 # Create your models here.
 
 
@@ -11,10 +12,18 @@ class Country(models.Model):
     countryID = models.IntegerField(primary_key=True)
     countryName = models.TextField(unique=True)
 
+    def __str__(self):
+        return self.countryName
+
+
 class Province(models.Model):
     provinceID = models.IntegerField(primary_key=True)
     countryID = models.ForeignKey(Country, on_delete=models.DO_NOTHING, )
     provinceName = models.TextField()
+
+    def __str__(self):
+        return self.provinceName
+
 
 class City(models.Model):
     cityID = models.IntegerField(primary_key=True)
@@ -22,43 +31,63 @@ class City(models.Model):
     countryName = models.ForeignKey(Country, on_delete=models.DO_NOTHING, )
     provinceID = models.ForeignKey(Province, on_delete=models.DO_NOTHING, )
 
+    def __str__(self):
+        return self.cityName
+
+
 class PropertyCategory(models.Model):
     propertyCategory = models.IntegerField(primary_key=True)
-    Single = models.CharField(max_length=200)
-    House = models.CharField(max_length=200)
-    Attached = models.CharField(max_length=200)
-    House = models.CharField(max_length=200)
-    Town = models.CharField(max_length=200)
-    House = models.CharField(max_length=200)
-    Appartment = models.CharField(max_length=200)
-    Store = models.CharField(max_length=200)
-    Farm = models.CharField(max_length=200)
-    Factory = models.CharField(max_length=200)
-    Mall = models.CharField(max_length=200)
-    Building = models.CharField(max_length=200)
+    SingleHouse = models.BooleanField(default=True)
+    AttachedHouse = models.BooleanField()
+    memberName = models.CharField(max_length=200)
+    TownHouse = models.BooleanField()
+    Appartment = models.BooleanField()
+    Store = models.BooleanField()
+    Farm = models.BooleanField()
+    Factory = models.BooleanField()
+    Mall = models.BooleanField()
+    Building = models.BooleanField()
     Other = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.propertyCategory
+
+
 class Property_Sector(models.Model):
     propertySector = models.IntegerField(primary_key=True)
-    Private = models.CharField(max_length=200)
-    Residential = models.CharField(max_length=200)
-    Commerical = models.CharField(max_length=200)
-    Goevernment = models.CharField(max_length=200)
-    Rural = models.CharField(max_length=200)
+    Private = models.BooleanField(default=True)
+    Residential = models.BooleanField()
+    Commerical = models.BooleanField()
+    Goevernment = models.BooleanField()
+    Rural = models.BooleanField()
     Other = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.propertySector
 
 
 class Property_Facing(models.Model):
     propertyFacing = models.IntegerField(primary_key=True)
-    North = models.CharField(max_length=200)
-    South = models.CharField(max_length=200)
-    East = models.CharField(max_length=200)
-    West = models.CharField(max_length=200)
+    North = models.BooleanField(default=True)
+    South = models.BooleanField()
+    East = models.BooleanField()
+    West = models.BooleanField()
     Other = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.propertyFacing
+
+
 class PropertyImages(models.Model):
     propertyImageID = models.IntegerField(primary_key=True)
-    #propertyID = models.ForeignKey(Property, on_delete=models.DO_NOTHING)
-    propertyImage = models.BinaryField(blank=True)
+    # propertyID = models.ForeignKey(Property, on_delete=models.DO_NOTHING)
+    propertyImage = models.ImageField(blank=True, null=True)
     propertyImageDescription = models.TextField()
+
+    def __str__(self):
+        return self.propertyImageID
+
+
 class Property(models.Model):
     propertyID = models.IntegerField(primary_key=True, null=False)
     propertyTitle = models.CharField(max_length=255)
@@ -82,8 +111,5 @@ class Property(models.Model):
     propertyAskingPrice = models.FloatField(null=False)
     propertySellingPrice = models.FloatField(null=False)
 
-
-
-
     def __str__(self):
-        return self.title
+        return self.propertyTitle
