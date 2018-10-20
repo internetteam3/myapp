69bf3235b7f6b701fe10d06aec5276892a82701f
Neel Shah <neel.4747@gmail.com>
2018-10-19 23:53:38 -0400

    necessary changes

--- myapp/admin.py
+++ myapp/admin.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 from .models import Property, PropertyCategory, Property_Sector, Property_Facing,PropertyImages, Country, Province, City
 # Register your models here.
 admin.site.register(Property)
+admin.site.register(PropertyCategory)
+admin.site.register(Property_Sector)
+admin.site.register(Property_Facing)
 admin.site.register(PropertyImages)
 admin.site.register(Country)
 admin.site.register(Province)
