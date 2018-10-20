5181a0834303934fdaa295527161bcf961b18d94
Neel Shah <neel.4747@gmail.com>
2018-10-19 21:56:15 -0400

    changes for mapping

--- S1_G3_Fall2018/urls.py
+++ S1_G3_Fall2018/urls.py
@@ -17,14 +17,20 @@ from django.contrib import admin
 from django.urls import path
 from myapp import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls import url, include
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+                  path('admin/', admin.site.urls),
 
-    path('', views.home, name='home'),
+                  path('', views.home, name='home'),
 
-    path('about', views.about, name='about'),
+                  path('about', views.about, name='about'),
 
-    path('sportEquip', views.sportEquip, name='sportEquip'),
+                  path('sportEquip', views.sportEquip, name='sportEquip'),
 
-    path('properties', views.properties, name='properties'),
-]
+                  path('properties', views.properties, name='properties'),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Property, PropertyCategory, Property_Sector, Property_Facing,PropertyImages, Country, Province, City
+# Register your models here.
+admin.site.register(Property)
+admin.site.register(PropertyImages)
+admin.site.register(Country)
+admin.site.register(Province)
+admin.site.register(City)
