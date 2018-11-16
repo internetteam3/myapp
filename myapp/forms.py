c3b4df2f3e21ed09a82195a5ac5aba89b34fa65c
dhavanmehta <mehta125@uwindsor.ca>
2018-11-16 18:07:14 -0500

    updated base of forms

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,24 +2,35 @@ from django import forms
 
 from .models import Users, Password, RoleCode, PermissionType, RolePermission, RolePermissionDetail
 
-
+class DateInput(forms.DateInput):
+    input_type = 'date'
 class UsersForm(forms.ModelForm):
     class Meta:
         model = Users
         fields = ['firstName', 'lastName', 'email']
+        labels = {
+            'firstName':'First Name',
+            'lastName':'Last Name'
+        }
         widgets = {
             'firstName': forms.TextInput(attrs={'class': 'single-input'}),
             'lastName': forms.TextInput(attrs={'class': 'single-input'}),
-            'email':forms.EmailInput(attrs={'class': 'single-input'})
+            'email':forms.EmailInput(attrs={'class': 'single-input' })
         }
 
 class PasswordForm(forms.ModelForm):
     class Meta:
         model = Password
         fields = ['userName', 'encryptedPassword', 'userAccountExpiryDate']
+        labels = {
+            'userName':'User Name',
+            'encryptedPassword':'Encrypted Password',
+            'userAccountExpiryDate':'Account Expiry Date'
+        }
         widgets = {
             'userName': forms.TextInput(attrs={'class': 'single-input'}),
-            'encryptedPassword': forms.PasswordInput(attrs={'class': 'single-input'})
+            'encryptedPassword': forms.PasswordInput(attrs={'class': 'single-input'}),
+            'userAccountExpiryDate': DateInput()
 
         }
 class RoleCodeForm(forms.ModelForm):
