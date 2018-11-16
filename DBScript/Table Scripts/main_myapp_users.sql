d2bae71aab84f60c9fa3ebf8798f900fd224f911
dhavanmehta <mehta125@uwindsor.ca>
2018-11-16 18:20:06 -0500

    backup files

---
+++ DBScript/Table Scripts/main_myapp_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO myapp_users (firstName, lastName, email, user_ID) VALUES ('neel', 'shah', 'neel@gmail.com', 1);
+INSERT INTO myapp_users (firstName, lastName, email, user_ID) VALUES ('dhavan', 'mehta', 'dhavan@gmail.com', 2);
